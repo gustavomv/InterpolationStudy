6fbc06b30085b8536c2ae1f51592aa72fde19298
Gustavo Moraes Vasconcelos <gmv_facul@hotmail.com>
2018-07-14 18:05:10 -0300

    Update sinc.m

--- Audio/SINC Interpolation/sinc.m
+++ Audio/SINC Interpolation/sinc.m
@@ -1,9 +1,17 @@
-[AudioSample Fs] = audioread('Super Mario World Music.mp3');
+[AudioSample Fs] = audioread('Super Mario World Music.wav');
 Audio = transpose(AudioSample);
 M = 5;
 
 DownS = decimate(Audio, M);
 Sinc = resampleSINC(DownS, M);
+
+AudioF = (0:length(AudioSample)-1)*100/length(AudioSample); %Frequency vector
+SincF = (0:length(Sinc)-1)*100/length(Sinc);
+
+AudioDFT = fft(AudioSample);
+SincDFT = fft(Sinc);
+AudioMag = abs(AudioDFT);
+SincMag = abs(SincDFT);
 
 figure(1);
 subplot(2,1,1);
@@ -25,3 +33,11 @@ lgd2 = legend('Input Signal', 'SINC Interpolation');
 lgd2.FontSize = 10;
 lgd2.FontWeight = 'bold'
 grid on
+
+figure(2);
+plot(AudioF, AudioMag, 'blue'); hold on
+plot(SincF, SincMag, 'red'); hold off
+lgd3 = legend('Input Signal', 'SINC Interpolation');
+lgd3.FontSize = 10;
+lgd3.FontWeight = 'bold'
+title('Magnitude');
