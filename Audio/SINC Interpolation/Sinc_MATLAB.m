1b0d205506285dba3c8ab195b0270fdf79af8acc
Gustavo Moraes Vasconcelos <gmv_facul@hotmail.com>
2018-07-15 16:16:01 -0300

    Update Sinc_MATLAB.m

--- Audio/SINC Interpolation/Sinc_MATLAB.m
+++ Audio/SINC Interpolation/Sinc_MATLAB.m
@@ -1,43 +1,73 @@
-[AudioSample Fs] = audioread('Super Mario World Music.wav');
+[AudioSample Fs] = audioread('Register.wav');
 Audio = transpose(AudioSample);
+
 M = 5;
+n = numel(Audio);
 
 DownS = decimate(Audio, M);
 Sinc = resampleSINC(DownS, M);
+Sinc = Sinc(1:n);
 
-AudioF = (0:length(AudioSample)-1)*100/length(AudioSample); %Frequency vector
-SincF = (0:length(Sinc)-1)*100/length(Sinc);
+%------------------ Analise das Metricas ----------------------------%
+ME = abs(Audio - Sinc);
 
-AudioDFT = fft(AudioSample);
-SincDFT = fft(Sinc);
-AudioMag = abs(AudioDFT);
-SincMag = abs(SincDFT);
+aux = 0;
+for i=1:n
+  aux = aux + abs(Audio(1,i) - Sinc(1,i));
+end
+MAE = aux/n;
 
-figure(1);
-subplot(2,1,1);
-plot(AudioSample, 'blue');hold on
-plot(DownS, 'red');hold off
-xlabel('Time(s)');
-ylabel('Amplitude');
-lgd = legend('Input Signal', 'Downsampled');
-lgd.FontSize = 10;
-lgd.FontWeight = 'bold'
-grid on
+aux = 0;
+for i=1:n
+  aux = aux + (abs(Audio(1,i) - Sinc(1,i)))^2;
+end
+MSE = aux/n;
+RMSE = sqrt(MSE);
 
-subplot(2,1,2)
-plot(AudioSample, 'blue');hold on
-plot(Sinc, 'red');hold off
-xlabel('Time(s)');
-ylabel('Amplitude');
-lgd2 = legend('Input Signal', 'SINC Interpolation');
-lgd2.FontSize = 10;
-lgd2.FontWeight = 'bold'
-grid on
+aux = 0;
+for i=1:n
+  aux = aux + Audio(1,i)^2;
+end 
+NMSE = (MSE*n)/aux;
 
-figure(2);
-plot(AudioF, AudioMag, 'blue'); hold on
-plot(SincF, SincMag, 'red'); hold off
-lgd3 = legend('Input Signal', 'SINC Interpolation');
-lgd3.FontSize = 10;
-lgd3.FontWeight = 'bold'
-title('Magnitude');
+SNR = 10*log10(aux/(MSE*n));
+
+%-----------------------------------------------------------------------%
+
+%AudioF = (0:length(AudioSample)-1)*100/length(AudioSample); %Frequency vector
+%SincF = (0:length(Sinc)-1)*100/length(Sinc);
+
+%AudioDFT = fft(AudioSample);
+%SincDFT = fft(Sinc);
+%AudioMag = abs(AudioDFT);
+%SincMag = abs(SincDFT);
+
+%figure(1);
+%subplot(2,1,1);
+%plot(AudioSample, 'blue');hold on
+%plot(DownS, 'red');hold off
+%xlabel('Time(s)');
+%ylabel('Amplitude');
+%lgd = legend('Input Signal', 'Downsampled');
+%lgd.FontSize = 10;
+%lgd.FontWeight = 'bold'
+%grid on
+
+%subplot(2,1,2)
+%plot(AudioSample, 'blue');hold on
+%plot(Sinc, 'red');hold off
+%xlabel('Time(s)');
+%ylabel('Amplitude');
+%lgd2 = legend('Input Signal', 'SINC Interpolation');
+%lgd2.FontSize = 10;
+%lgd2.FontWeight = 'bold'
+%grid on
+
+%figure(2);
+%plot(ME);
+%plot(AudioF, AudioMag, 'blue'); hold on
+%plot(SincF, SincMag, 'red'); hold off
+%lgd3 = legend('Input Signal', 'SINC Interpolation');
+%lgd3.FontSize = 10;
+%lgd3.FontWeight = 'bold'
+%title('Magnitude');
