c64f135bd3891e116ccfe7e0a0f2db94168cc23e
Camila Novaes <novaes.csw@gmail.com>
2018-07-15 17:53:56 -0300

    Change gaussian filter

--- Image/interface.m
+++ Image/interface.m
@@ -11,24 +11,28 @@ im_gray = rgb2gray(image);
 %-------------
 % Filtro Passa baixa
 %-------------
-mpad = padarray(im_gray, [4 4], "symmetric");
-f_gauss = fspecial("gaussian", 9, 2);
-gauss_m = filter2(f_gauss, mpad, "valid");
-gauss_m = uint8(gauss_m);
+%mpad = padarray(im_gray, [4 4], "symmetric");
+%f_gauss = fspecial("gaussian", 9, 2);
+%gauss_m = filter2(f_gauss, mpad, "valid");
+%gauss_m = uint8(gauss_m);
+
+gauss_m2 = imsmooth(im_gray,"Gaussian");
 
 %-------------
 % Downsampling
 %-------------
-m = imresize(gauss_m, 1);
+m = imresize(gauss_m2, 0.5);
+%mo = ds(image, 2, 2);
+%mo= uint8(mo);
 
 %-------------
 % Histograma
 %-------------
 %figure
-%imhist(m);
-
-%figure
-%imhist(gauss_m);
+%subplot(2,2,1);imhist(im_gray);title('Original');
+%subplot(2,2,2);imhist(m);title('Downsampling');
+%subplot(2,2,3);imhist(gauss_m);title('Gaussian1');
+%subplot(2,2,4);imhist(gauss_m2);title('Gaussian2 imsmooth');
 
 %-------------
 % Filtros
@@ -40,9 +44,10 @@ n = nn(m, im_size(1), im_size(2));
 %-------------
 % Imagem
 %-------------
+%figure
 subplot(2,2,1);imagesc(im_gray);colormap(gray);title('Original Gray');
 subplot(2,2,2);imagesc(m);colormap(gray);title('Lowpass and Downsampling');
-subplot(2,2,3);imagesc(b);colormap(gray);title('Neighbour');
-subplot(2,2,4);imagesc(n);colormap(gray);title('Bilinear');
+subplot(2,2,3);imagesc(n);colormap(gray);title('Neighbour');
+subplot(2,2,4);imagesc(b);colormap(gray);title('Bilinear');
 
-clear all;
+%clear all;
